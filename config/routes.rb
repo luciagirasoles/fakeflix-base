02716256445dc203da2ac16a87e19fbe14737f06
mayra navarro <luciagirasoles@gmail.com>
2019-04-09 17:41:53 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
     #GET /api/movies?filter=value  query tring ?key1=value1&key2=value2
-    resources :movies, only: [:index, :show]
+    resources :movies, only: [:index, :show] do
+      patch '/playback', to: 'movies#update'
+    end
 
     resources :series, only: [:index, :show]
-    resources :episodes, only: [:show]
+    resources :episodes, only: [:show] do
+      patch '/playback', to: 'episodes#update'
+    end
     post '/rents/:resource/:id',	to: 'rentals#create'
+    get '/rents', to: 'rentals#index'
   end
     
 
