d2489f596f0c889bcd702e6c103bd1d39f1fddad
mayra navarro <luciagirasoles@gmail.com>
2019-04-09 18:16:23 -0500

    create index in series

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,3 +1,16 @@
 class SeriesController < ApplicationController
+  def index
+    @series = Serie.all
+    if params[:filter] #key desp de signo de interrogacion
+      @series = Serie.where(status: params[:filter]) # funciona con nro o con nombre
+    end
+    render json: @series.as_json(methods: [ :rented])
+  end
+
+  def show
+  end
+
+  def update
+  end
 
 end
