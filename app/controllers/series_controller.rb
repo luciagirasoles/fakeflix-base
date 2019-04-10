b46048c396134695cbaa3d4c04dfd6c4c687a346
mayra navarro <luciagirasoles@gmail.com>
2019-04-10 13:14:30 -0500

    configurate show method on Series adding duration method duration on Serie model configurating  show action

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -4,10 +4,18 @@ class SeriesController < ApplicationController
     if params[:filter] #key desp de signo de interrogacion
       @series = Serie.where(status: params[:filter]) # funciona con nro o con nombre
     end
-    render json: @series.as_json(methods: [ :rented])
+    render json: @series.as_json(methods: [:rented])
   end
 
   def show
+    render json: Serie.find(params[:id]).as_json(
+      except: [:created_at, :updated_at], 
+      include: { 
+        episodes: { only: [:title, :duration]}
+      },
+      methods: [:rented, :duration]
+    )
+    
   end
 
   def update
--- app/models/serie.rb
+++ app/models/serie.rb
@@ -7,6 +7,9 @@ class Serie < ApplicationRecord
         self.rentals.any?
       end
 
+    def duration
+        self.episodes.sum(:duration)/60.0 #mins
+    end
 end
 
 # == Schema Information
@@ -21,4 +24,25 @@ end
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#
+# == Schema Information
+#
+# Table name: episodes
+#
+#  id          :bigint(8)        not null, primary key
+#  description :text
+#  duration    :integer
+#  progress    :integer          default(0)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  serie_id    :bigint(8)        indexed
+#
+# Indexes
+#
+#  index_episodes_on_serie_id  (serie_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (serie_id => series.id)
 #
