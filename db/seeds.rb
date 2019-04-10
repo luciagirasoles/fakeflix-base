6a3bb892b78208d9099dbcc510de170d36a604f8
mayra navarro <luciagirasoles@gmail.com>
2019-04-10 12:39:30 -0500

    update seed Adding episodes data

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,10 +23,9 @@
 movie1 = Movie.create(title: 'Star Wars')
 movie2 = Movie.create(title: 'It')
 
+
 # == Schema Information
-#
 # Table name: rentals
-#
 #  id            :bigint(8)        not null, primary key
 #  paid_price    :integer
 #  rentable_type :string           indexed => [rentable_id]
@@ -34,9 +33,43 @@ movie2 = Movie.create(title: 'It')
 #  updated_at    :datetime         not null
 #  rentable_id   :bigint(8)        indexed => [rentable_type]
 #
-# Indexes
-#
-#  index_rentals_on_rentable_type_and_rentable_id  (rentable_type,rentable_id)
 #
 
 movie1.rentals.create(paid_price: 230)
+
+############################################
+# Table name: series
+#  id          :bigint(8)        not null, primary key
+#  description :text
+#  price       :integer
+#  rating      :integer
+#  status      :integer          default("comingsoon")
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+serie1 = Serie.create(title: 'Stranger Things', status: 'preorder')
+serie2 = Serie.create(title: 'Osmosis')
+serie3 = Serie.create(title: 'NCIS - NY')
+
+serie1.rentals.create(paid_price: 250)
+
+episode11 = Episode.create(title:'S1-Ep1 - Title 1', serie_id: serie1['id'], duration: 2400 ) #seconds
+episode12 = Episode.create(title:'S1-Ep2 - Title 2', serie_id: serie1['id'], duration: 2600 )
+episode13 = Episode.create(title:'S1-Ep3 - Title 3', serie_id: serie1['id'], duration: 1200 )
+
+episode21 = Episode.create(title:'S2-Ep1 - Title 1', serie_id: serie2['id'], duration: 1200 )
+episode22 = Episode.create(title:'S2-Ep2 - Title 2', serie_id: serie2['id'], duration: 2400 )
+
+
+# == Schema Information
+# Table name: episodes
+#  id          :bigint(8)        not null, primary key
+#  description :text
+#  duration    :integer
+#  progress    :integer          default(0)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  serie_id    :bigint(8)        indexed
